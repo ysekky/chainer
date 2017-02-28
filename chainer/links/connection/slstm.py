f40f10d97d2732394d53ddf5edd3faba6667277b
Yuya Unno <unnonouno@gmail.com>
2017-03-01 02:08:59 +0900

    Implemenet S-LSTM link

--- chainer/links/connection/slstm.py
+++ chainer/links/connection/slstm.py
@@ -1,4 +1,4 @@
-from chainer.functions.activation import lstm
+from chainer.functions.activation import slstm
 from chainer import link
 from chainer.links.connection import linear
 from chainer import variable
@@ -6,62 +6,47 @@ from chainer import variable
 
 class StatelessSLSTM(link.Chain):
 
-    """Fully-connected LSTM layer.
+    """Stateless S-LSTM layer.
 
-    This is a fully-connected LSTM layer as a chain. Unlike the
-    :func:`~chainer.functions.lstm` function, which is defined as a stateless
-    activation function, this chain holds upward and lateral connections as
-    child links.
-
-    It also maintains *states*, including the cell state and the output
-    at the previous time step. Therefore, it can be used as a *stateful LSTM*.
+    This is a fully-connected S-LSTM layer as a chain. Unlike the
+    :func:`~chainer.functions.slstm` function, which is defined as a stateless
+    activation function.
 
     Args:
         in_size (int): Dimensionality of input vectors.
         out_size (int): Dimensionality of output vectors.
 
     Attributes:
-        upward (chainer.links.Linear): Linear layer of upward connections.
-        lateral (chainer.links.Linear): Linear layer of lateral connections.
-        c (chainer.Variable): Cell states of LSTM units.
-        h (chainer.Variable): Output at the previous timestep.
+        W (chainer.links.Linear): Linear layer of upward connections.
 
     """
     def __init__(self, in_size, out_size):
-        super(SLSTM, self).__init__(
-            upward1=linear.Linear(in_size, 4 * out_size),
-            upward2=linear.Linear(in_size, 4 * out_size),
-            lateral1=linear.Linear(out_size, 4 * out_size, nobias=True),
-            lateral2=linear.Linear(out_size, 4 * out_size, nobias=True),
+        super(StatelessSLSTM, self).__init__(
+            W1=linear.Linear(out_size, 4 * out_size),
+            W2=linear.Linear(out_size, 4 * out_size),
         )
         self.state_size = out_size
 
-
-    def _make_input(self, c, h, x, upward, lateral):
-        lstm_in = upward(x)
-        if h is not None:
-            lstm_in += lateral(h)
+    def _make_input(self, c, h, W):
+        lstm_in = W(h)
         if c is None:
-            c = variable.Variable(
-                self.xp.zeros(
-                    (len(x1.data), self.state_size), dtype=x.data.dtype),
-                volatile='auto')
+            c = self.xp.zeros((len(h.data), self.state_size), dtype=h.dtype)
         return c, lstm_in
 
-
-    def __call__(self, c1, h1, c2, h2, x1, x2):
-        """Updates the internal state and returns the LSTM outputs.
+    def __call__(self, c1, h1, c2, h2):
+        """Returns S-LSTM output.
 
         Args:
-            x (~chainer.Variable): A new batch from the input sequence.
+            c1 (~chainer.Variable): Left cell state of S-LSTM units.
+            h1 (~chainer.Variable): Left hidden state.
+            c2 (~chainer.Variable): Right cell state.
+            h2 (~chainer.Variable): Right hidden state.
 
         Returns:
-            ~chainer.Variable: Outputs of updated LSTM units.
+            ~chainer.Variable: Outputs of updated S-LSTM units.
 
         """
-        c1, lstm_in1 = self._make_input(
-            c1, h1, x1, self.upward1, self.lateral1)
-        c2, lstm_in2 = self._make_input(
-            c2, h2, x2, self.upward2, self.lateral2)
+        c1, lstm_in1 = self._make_input(c1, h1, self.W1)
+        c2, lstm_in2 = self._make_input(c2, h2, self.W2)
         c, h = slstm.slstm(c1, c2, lstm_in1, lstm_in2)
         return c, h
